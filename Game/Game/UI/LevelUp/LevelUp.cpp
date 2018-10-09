43f70d619320cc257c33bfdf84a0cd05472742a2
村上恒平 <mikan.zuki319@gmail.com>
2018-10-09 16:01:46 +0900

    levelUp表示

---
+++ Game/Game/UI/LevelUp/LevelUp.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "LevelUp.h"
+#include "../../Player/Player.h"
+
+CLevelUp::CLevelUp()
+{
+}
+
+
+CLevelUp::~CLevelUp()
+{
+}
+
+void CLevelUp::Init()
+{
+	//プレイヤーHPのロード
+	m_playerLevelUpTexture.Load(L"Assets/sprite/levelup.png");
+	m_playerLevelUpSprite.Init(&m_playerLevelUpTexture);
+	m_playerLevelUpSprite.SetPosition({ m_playerLevelUpPos });
+	m_playerLevelUpSprite.SetSize({ m_playerLevelUpSize});
+	m_playerLevelUpSprite.SetAlpha(0.0f);
+	m_playerLevelValue = GetPlayer().GetStatus().Level;
+}
+
+void CLevelUp::Update()
+{
+	//レベルが変わったら
+	if (GetPlayer().GetStatus().Level != m_playerLevelValue)
+	{
+		m_playerLevelValue = GetPlayer().GetStatus().Level;
+		m_playerLevelUpAlpha = 1.0f;
+		m_playerLevelUpSprite.SetAlpha(m_playerLevelUpAlpha);
+	}
+
+	m_playerLevelUpAlpha -= 0.1f;//GameTime().GetDeltaFrameTime() / 5.0f;
+	//m_playerLevelUpSprite.SetAlpha(m_playerLevelUpAlpha);
+
+	
+}
+
+void CLevelUp::Draw()
+{
+	if (m_playerLevelUpAlpha <= 0.0f)
+	{
+		return;
+	}
+	m_playerLevelUpSprite.Draw();
+}
