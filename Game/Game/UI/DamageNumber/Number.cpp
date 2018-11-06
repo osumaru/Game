4ed741b9ef8705ca900bdb0c369eded14ab83c60
konishi <a>
2018-11-06 16:38:25 +0900

    敵に攻撃したときにフェードで使っっているテクスチャがでるバグを修正

--- Game/Game/UI/DamageNumber/Number.cpp
+++ Game/Game/UI/DamageNumber/Number.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "../DamageNumber/Number.h"
+#include "Number.h"
 
 void CNumber::Init(CVector2 numPos, CVector2 numSize)
 {
@@ -19,7 +19,5 @@ void CNumber::Update()
 
 void CNumber::AfterDraw()
 {
-	Engine().SetAlphaBlendState(enAlphaBlendStateAdd);
 	m_number[m_num].Draw();
-	Engine().SetAlphaBlendState(enAlphaBlendStateNone);
 }
