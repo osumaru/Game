c921360861b793e224ebf32fa2e7317ce744815e
konishi <a>
2018-11-13 15:39:24 +0900

    ダメージ計算の処理をDamageNumberクラスに移動させた

--- Game/Game/Enemy/EnemyState/EnemyDamage.cpp
+++ Game/Game/Enemy/EnemyState/EnemyDamage.cpp
@@ -2,12 +2,7 @@
 #include "EnemyDamage.h"
 #include "../IEnemy.h"
 #include "../../Player/Player.h"
-#include "../../Camera/GameCamera.h"
 #include "../../UI/DamageNumber/DamageNumber.h"
-
-CEnemyDamage::~CEnemyDamage()
-{
-}
 
 bool CEnemyDamage::Start()
 {
@@ -15,19 +10,7 @@ bool CEnemyDamage::Start()
 	m_enemy->PlayAnimation(CEnemyState::enState_Damage);
 
 	m_damageNumber = New<CDamageNumber>(0);
-	m_damageNumber->Init();
-
-	//ダメージ計算
-	int playerStrength = GetPlayer().GetStatus().Strength;
-	int enemyDefence = m_enemy->GetStatus().Defense;
-	int damage = playerStrength - enemyDefence;
-	m_enemy->HpDamage(damage);
-	m_damageNumber->DamageCalculation(damage);
-
-	CMatrix leftShoulderMatrix = m_enemy->GetBoneWorldMatrix(L"LeftShoulder");
-	m_damagePos.x = leftShoulderMatrix.m[3][0];
-	m_damagePos.y = leftShoulderMatrix.m[3][1];
-	m_damagePos.z = leftShoulderMatrix.m[3][2];
+	m_damageNumber->Init(m_enemy);
 
 	//ダメージを受けたフラグを戻す
 	m_enemy->SetIsDamage(false);
@@ -45,21 +28,6 @@ bool CEnemyDamage::Start()
 
 void CEnemyDamage::Update()
 {
-	CMatrix viewMatrix = GetGameCamera().GetViewMatrix();
-	CMatrix projectionMatrix = GetGameCamera().GetProjectionMatrix();
-	//ビュー変換
-	CVector4 viewPosition = m_damagePos;
-	viewMatrix.Mul(viewPosition);
-	//プロジェクション変換
-	CVector4 projectionPosition = viewPosition;
-	projectionMatrix.Mul(projectionPosition);
-	projectionPosition = projectionPosition / projectionPosition.w;
-	//スクリーン変換
-	CVector2 screenPosition;
-	screenPosition.x = (1.0f + projectionPosition.x) / 2.0f * FrameBufferWidth() - (FrameBufferWidth() / 2.0f);
-	screenPosition.y = (1.0f + projectionPosition.y) / 2.0f * FrameBufferHeight() - (FrameBufferHeight() / 2.0f);
-	m_damageNumber->SetPosition(screenPosition);
-
 	CVector3 moveSpeed = m_enemy->GetMoveSpeed();
 	CVector3 knockBack = moveSpeed;
 	knockBack *= GameTime().GetDeltaFrameTime();
--- Game/Game/Enemy/EnemyState/EnemyDamage.h
+++ Game/Game/Enemy/EnemyState/EnemyDamage.h
@@ -20,7 +20,7 @@ public:
 	}
 
 	//デストラクタ
-	~CEnemyDamage();
+	~CEnemyDamage() {};
 
 	//更新する前に一度だけ呼ばれる
 	bool Start() override;
@@ -31,7 +31,7 @@ public:
 	//解放
 	void Release() override;
 private:
-	CVector3		m_damagePos;				//ダメージ表示させる座標
+	//CVector3		m_damagePos;				//ダメージ表示させる座標
 	CDamageNumber*	m_damageNumber = nullptr;	//ダメージ表示
 	CVector3		m_knockBack;
 	const float		m_knockBackSpeed = 1.0f;
