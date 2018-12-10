e3e21a99fa098b41b9bdacdf04f453e3e76f1c34
yuuki <kbc16b09@stu.kawahara.ac.jp>
2018-12-10 11:05:39 +0900

    3Dサウンド実装 エフェクトエンジンにコメントを追加

--- osumaruEngine/Effect/EffectEngine.h
+++ osumaruEngine/Effect/EffectEngine.h
@@ -1,28 +1,39 @@
 #pragma once
 #include "Effect.h"
+//エフェクトエンジン
 
 class CEffectEngine : Uncopyable
 {
 public:
+	//初期化
 	void Init();
 
+	//更新
 	void Update();
 
+	//描画
 	void Draw();
 
+	//エフェクト描画用のカメラを設定
 	void SetCamera(CCamera* camera)
 	{
 		m_pCamera = camera;
 	}
 
+	/*
+	エフェクトを再生
+	effect	再生するエフェクトのインスタンス
+	
+	*/
 	Effekseer::Handle Play(CEffect& effect);
 
+	//エフェクトマネージャーを取得
 	Effekseer::Manager* GetEffectManager()
 	{
 		return m_manager;
 	}
 private:
-	CCamera*						m_pCamera = nullptr;
-	Effekseer::Manager*				m_manager = nullptr;
-	EffekseerRenderer::Renderer*	m_renderer = nullptr;
+	CCamera*						m_pCamera = nullptr;	//描画に使うカメラ
+	Effekseer::Manager*				m_manager = nullptr;	//マネージャー
+	EffekseerRenderer::Renderer*	m_renderer = nullptr;	//レンダラー
 };
