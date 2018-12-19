db736358c47a321b9033b2e7ddc5807a059fe725
yuuki <kbc16b09@stu.kawahara.ac.jp>
2018-12-20 01:31:12 +0900

    マップにいろいろと追加(敵や建物やショップなど

--- Game/Game/Map/alpha2.h
+++ Game/Game/Map/alpha2.h
@@ -5,68 +5,12 @@
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagPlayer, //タグ
 },
-//Building1
-{
-	L"Building1",
-	CVector3(-128.50f, 0.00f, -203.70f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
 //Map4
 {
 	L"Map4",
 	CVector3(0.00f, 0.00f, 0.00f),             //平行移動
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagTerrain, //タグ
-},
-//Building2
-{
-	L"Building2",
-	CVector3(-5.80f, 0.00f, 119.90f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building3
-{
-	L"Building3",
-	CVector3(-128.00f, 0.00f, -239.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building4
-{
-	L"Building4",
-	CVector3(90.30f, 0.00f, -5.50f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building5
-{
-	L"Building5",
-	CVector3(115.10f, 0.00f, 138.50f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building1
-{
-	L"Building1",
-	CVector3(-128.50f, 0.00f, -170.70f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagUntagged, //タグ
-},
-//Building1
-{
-	L"Building1",
-	CVector3(-156.10f, 0.00f, -170.70f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building1
-{
-	L"Building1",
-	CVector3(-187.80f, 0.00f, -170.70f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
 },
 //ninja
 {
@@ -113,23 +57,9 @@
 //Shopgirl
 {
 	L"Shopgirl",
-	CVector3(-1.60f, 0.00f, -23.50f),             //平行移動
+	CVector3(-125.50f, 0.00f, -144.60f),             //平行移動
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagItemShop, //タグ
-},
-//Building2
-{
-	L"Building2",
-	CVector3(156.00f, 0.00f, 12.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building1
-{
-	L"Building1",
-	CVector3(76.40f, 0.00f, 80.60f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
 },
 //samurai
 {
@@ -169,7 +99,7 @@
 //Shopgirl
 {
 	L"Shopgirl",
-	CVector3(-6.67f, 0.00f, -23.50f),             //平行移動
+	CVector3(-130.57f, 0.00f, -144.60f),             //平行移動
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagWeaponShop, //タグ
 },
@@ -207,34 +137,6 @@
 	CVector3(-29.45f, 0.00f, 15.20f),             //平行移動
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagEnemyGroup, //タグ
-},
-//Building5
-{
-	L"Building5",
-	CVector3(0.60f, 0.00f, -233.50f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building5
-{
-	L"Building5",
-	CVector3(0.60f, 0.00f, -198.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building5
-{
-	L"Building5",
-	CVector3(34.00f, 0.00f, -198.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building5
-{
-	L"Building5",
-	CVector3(34.00f, 0.00f, -232.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
 },
 //Warrok
 {
@@ -320,34 +222,6 @@
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagEnemyGroup, //タグ
 },
-//Building1
-{
-	L"Building1",
-	CVector3(4.00f, 0.00f, -92.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building4
-{
-	L"Building4",
-	CVector3(82.10f, 0.00f, -122.80f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building3
-{
-	L"Building3",
-	CVector3(-52.00f, 0.00f, -169.00f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
-//Building2
-{
-	L"Building2",
-	CVector3(115.50f, 0.00f, -120.90f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-	enMapTagObstacle, //タグ
-},
 //Warrok
 {
 	L"Warrok",
@@ -389,4 +263,515 @@
 	CVector3(-3.20f, 0.00f, 175.60f),             //平行移動
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagEnemyGroup, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-128.50f, 0.00f, -203.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-5.80f, 0.00f, 119.90f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building3
+{
+	L"Building3",
+	CVector3(-128.00f, 0.00f, -239.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(90.30f, 0.00f, -5.50f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building5
+{
+	L"Building5",
+	CVector3(115.10f, 0.00f, 138.50f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-128.50f, 0.00f, -170.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-156.10f, 0.00f, -170.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-187.80f, 0.00f, -170.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(156.00f, 0.00f, 12.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(76.40f, 0.00f, 80.60f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building5
+{
+	L"Building5",
+	CVector3(0.60f, 0.00f, -233.50f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building5
+{
+	L"Building5",
+	CVector3(0.60f, 0.00f, -198.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building5
+{
+	L"Building5",
+	CVector3(34.00f, 0.00f, -198.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building5
+{
+	L"Building5",
+	CVector3(34.00f, 0.00f, -232.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(102.10f, 0.00f, -223.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(82.10f, 0.00f, -122.80f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building3
+{
+	L"Building3",
+	CVector3(-52.00f, 0.00f, -169.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(115.50f, 0.00f, -120.90f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-79.00f, 0.00f, 144.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(33.00f, 0.00f, -5.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-149.10f, 0.00f, 47.30f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-204.30f, 0.00f, 209.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-298.20f, 0.00f, 297.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-352.20f, 0.00f, 66.20f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(-270.00f, 0.00f, 211.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building5
+{
+	L"Building5",
+	CVector3(-352.00f, 0.00f, 203.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building3
+{
+	L"Building3",
+	CVector3(-93.00f, 0.00f, -45.60f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(-228.00f, 0.00f, 131.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-172.00f, 0.00f, -116.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-218.00f, 0.00f, -247.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-186.50f, 0.00f, -247.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-154.70f, 0.00f, -247.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-123.30f, 0.00f, -247.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(-317.00f, 0.00f, -245.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-194.30f, 0.00f, -387.90f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(-194.30f, 0.00f, -355.40f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(-170.30f, 0.00f, -25.50f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(-281.20f, 0.00f, -6.90f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building4
+{
+	L"Building4",
+	CVector3(139.60f, 0.00f, -170.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building2
+{
+	L"Building2",
+	CVector3(263.70f, 0.00f, -269.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building3
+{
+	L"Building3",
+	CVector3(-165.30f, 0.00f, 112.80f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagObstacle, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-61.90f, 0.00f, -44.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//Building1
+{
+	L"Building1",
+	CVector3(-38.60f, 0.00f, -112.50f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(-246.50f, 0.00f, 81.10f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(-235.60f, 0.00f, 82.10f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(-235.10f, 0.00f, 66.60f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(-245.90f, 0.00f, 68.80f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(-240.00f, 0.00f, 75.30f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagEnemyGroup, //タグ
+},
+//samurai
+{
+	L"samurai",
+	CVector3(-183.30f, 0.00f, 165.10f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagSamurai, //タグ
+},
+//samurai
+{
+	L"samurai",
+	CVector3(-194.00f, 0.00f, 149.20f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagSamurai, //タグ
+},
+//samurai
+{
+	L"samurai",
+	CVector3(-175.10f, 0.00f, 155.50f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagSamurai, //タグ
+},
+//samurai
+{
+	L"samurai",
+	CVector3(-182.30f, 0.00f, 144.90f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagSamurai, //タグ
+},
+//samurai
+{
+	L"samurai",
+	CVector3(-184.40f, 0.00f, 155.40f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagEnemyGroup, //タグ
+},
+//Warrok
+{
+	L"Warrok",
+	CVector3(50.90f, 0.00f, 157.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWarrok, //タグ
+},
+//Warrok
+{
+	L"Warrok",
+	CVector3(59.50f, 0.00f, 157.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWarrok, //タグ
+},
+//Warrok
+{
+	L"Warrok",
+	CVector3(51.30f, 0.00f, 149.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWarrok, //タグ
+},
+//Warrok
+{
+	L"Warrok",
+	CVector3(58.70f, 0.00f, 148.40f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWarrok, //タグ
+},
+//Warrok
+{
+	L"Warrok",
+	CVector3(54.00f, 0.00f, 153.90f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagEnemyGroup, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(43.70f, 0.00f, -40.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(51.20f, 0.00f, -38.10f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(45.90f, 0.00f, -44.60f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(53.90f, 0.00f, -43.60f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagNinja, //タグ
+},
+//ninja
+{
+	L"ninja",
+	CVector3(49.60f, 0.00f, -41.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagEnemyGroup, //タグ
+},
+//zombi
+{
+	L"zombi",
+	CVector3(-263.00f, 0.00f, -143.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagZombie, //タグ
+},
+//zombi
+{
+	L"zombi",
+	CVector3(-270.80f, 0.00f, -154.70f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagZombie, //タグ
+},
+//zombi
+{
+	L"zombi",
+	CVector3(-271.70f, 0.00f, -147.30f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagZombie, //タグ
+},
+//zombi
+{
+	L"zombi",
+	CVector3(-265.80f, 0.00f, -149.40f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagEnemyGroup, //タグ
+},
+//Shopgirl
+{
+	L"Shopgirl",
+	CVector3(-12.70f, 0.00f, 34.90f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWeaponShop, //タグ
+},
+//Shopgirl
+{
+	L"Shopgirl",
+	CVector3(-6.70f, 0.00f, 34.90f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagItemShop, //タグ
+},
+//Shopgirl
+{
+	L"Shopgirl",
+	CVector3(62.80f, 0.00f, -114.80f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWeaponShop, //タグ
+},
+//Shopgirl
+{
+	L"Shopgirl",
+	CVector3(62.80f, 0.00f, -125.00f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagItemShop, //タグ
+},
+//Shopgirl
+{
+	L"Shopgirl",
+	CVector3(-183.00f, 0.00f, -37.70f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagWeaponShop, //タグ
+},
+//Shopgirl
+{
+	L"Shopgirl",
+	CVector3(-183.00f, 0.00f, -27.20f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagItemShop, //タグ
 },
