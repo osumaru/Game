2bf147e1c7402d33bff141e264b0d9bf20f4734d
yuuki <kbc16b09@stu.kawahara.ac.jp>
2018-12-21 17:34:52 +0900

    ショップにアニメーションを再生させた エネミーの起き上がりモーションの回転がずれているのを修正

--- Game/Game/NPC/Shop/WeaponShop.cpp
+++ Game/Game/NPC/Shop/WeaponShop.cpp
@@ -21,7 +21,11 @@ void CWeaponShop::Init(const CVector3 position, const CQuaternion rotation)
 	//LoadFile(L"Assets/sprite/shopData");
 	m_position = position;
 	m_rotation = rotation;
-	m_skinModel.Load(L"Assets/modelData/Shopgirl.cmo", NULL);
+	wchar_t* animClip[] = { L"Assets/modelData/ShopgirlStand.tka" };
+	m_animation.Init(animClip, 1);
+	m_animation.SetLoopFlg(0, true);
+	m_skinModel.Load(L"Assets/modelData/Shopgirl.cmo", &m_animation);
+	m_animation.Play(0);
 
 	//テクスチャ及びスプライト関係の初期化
 	{
