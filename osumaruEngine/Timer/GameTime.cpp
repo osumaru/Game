6c35f07a080b35cf1b708eb60492161060db7cbe
yuuki <kbc16b09@stu.kawahara.ac.jp>
2018-12-24 23:26:56 +0900

    フェイドの修正 ゲームタイムのスローになる機能を追加

--- osumaruEngine/Timer/GameTime.cpp
+++ osumaruEngine/Timer/GameTime.cpp
@@ -1,2 +1,23 @@
 #include "engineStdafx.h"
 #include "GameTime.h"
+
+
+void CGameTime::Update(CStopWatch& sw)
+{
+	float limitTime = 1.0f / 60.0f;
+	float deltaTime = sw.GetElapsedTime();
+	if (deltaTime < limitTime)
+	{
+		DWORD sleepTime = max(0.0, limitTime * 1000.0 - (DWORD)sw.GetElapsedTimeMill());
+		Sleep(sleepTime);
+		deltaTime = limitTime;
+	}
+
+	m_frameDeltaTime = deltaTime;
+	if (0.0f < m_slowTime)
+	{
+		m_frameDeltaTime *= m_slowScale;
+		m_slowTime -= deltaTime;
+	}
+	
+}
