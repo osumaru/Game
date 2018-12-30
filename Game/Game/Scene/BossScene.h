1f2dd2432e96bb4d4af27a0ff7edec75528fc38d
konishi <a>
2018-12-31 00:44:51 +0900

    ボスシーンを作成してゲームシーンから遷移できるようにした

---
+++ Game/Game/Scene/BossScene.h
@@ -0,0 +1,38 @@
+#pragma once
+
+class Map;
+class CWeaponSelect;
+class CMiniMap;
+class CPlayerHp;
+class CLevelUp;
+class CMenu;
+class CResult;
+class CGameSound;
+
+class CBossScene : public IGameObject
+{
+public:
+	//コンストラクタ
+	CBossScene() {};
+
+	//デストラクタ
+	~CBossScene() {};
+
+	//更新される前に一度だけ呼ばれる
+	bool Start() override;
+
+	//更新
+	void Update() override;
+
+	//死ぬ前に呼び出される
+	void BeforeDead() override;
+private:
+	Map*			m_map = nullptr;			//マップ
+	CWeaponSelect*	m_weaponSelect = nullptr;	//武器選択
+	//CMiniMap*		m_miniMap = nullptr;		//ミニマップ
+	CPlayerHp*		m_playerHp = nullptr;		//プレイヤーHP
+	CLevelUp*		m_levelUp = nullptr;		//レベルアップ表示
+	//CMenu*			m_menu = nullptr;			//メニュー
+	CResult*		m_result = nullptr;			//リザルト
+	CGameSound*		m_gameSound = nullptr;		//ゲームサウンド
+};
