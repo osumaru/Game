fef88b010b1d179a74a83e6a7717fae6e654f250
kbc16b15 <kbc16b15@stu.kawahara.ac.jp>
2019-01-15 12:24:11 +0900

    ボス攻撃アニメーションイベント使うように ボスステージドア付け ダメージアニメーションするようにした

--- Game/Game/Map/BossStage.h
+++ Game/Game/Map/BossStage.h
@@ -89,3 +89,24 @@
 	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
 	enMapTagMaw, //タグ
 },
+//CastleDoor
+{
+	L"CastleDoor",
+	CVector3(-10.83f, 10.33f, 6.57f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//CastleDoor2
+{
+	L"CastleDoor2",
+	CVector3(-9.60f, 10.38f, 6.70f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ),  //回転
+	enMapTagUntagged, //タグ
+},
+//CastleDoor2
+{
+	L"CastleDoor2",
+	CVector3(-12.07f, 10.36f, 6.70f),             //平行移動
+	CQuaternion(0.00f, 1.00f, 0.00f, 0.00f ),  //回転
+	enMapTagUntagged, //タグ
+},
