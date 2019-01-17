b3de0eb90da655c19b4ebe6c66bbc590da67543a
OsakanaSAN <kbc16b03@stu.kawahara.ac.jp>
2019-01-17 11:54:22 +0900

    モデルの追加と修正

--- Game/Game/Item/TreasureChest.cpp
+++ Game/Game/Item/TreasureChest.cpp
@@ -85,20 +85,32 @@ void CTreasureChest::DesideWeaponStatus()
 	wchar_t* itemName = L"ああああ";
 	wchar_t* textureFileName = L"Assets/sprite/item/Equip/Equip_2.png";
 	////////////////////////////////////////////////////////////////////
+	/*
+	調査報告。
+	EquipInventoryにあるm_equipListの情報が
+	EquipInventoryのInit時に書き変わってしまうバグがある。
+	wchar_t* itemName = L"ああああ";
+	wchar_t* textureFileName = L"Assets/sprite/item/Equip/Equip_2.png";
+	上の入れ方では書き変わらないが
+	weaponNumber = nItem->GetItemStatus_ItemId(num).WeaponType;
+	itemName = nItem->GetItemStatus_ItemId(num).ItemName;
+	では情報がなくなってしまった。
+	*/
+
 	//アイテムのデータを取得
 	//武器情報の取得
-	
-	//CEquipItem* nItem = GetSceneManager().GetFade()->GetLoadScene()->GetEquipItemData();
-	////↓これはレア度がNormalの武器からランダムで武器を取得するコード
-	//int num = nItem->GetNormalEquipItemList(Random().GetRandSInt() % nItem->GetNormalEquipItemListSize()) ;
-	////武器のタイプの取得
-	//weaponNumber = nItem->GetItemStatus_ItemId(num).WeaponType;
-	////武器の名前の取得
-	//itemName = nItem->GetItemStatus_ItemId(num).ItemName;
-	////武器のUI情報の取得(文字列)
-	//textureFileName = nItem->GetItemStatus(num).ItemSprite;
-	//weaponAttack = nItem->GetItemStatus_ItemId(num).ItemEffect;
-	
+	/*
+	CEquipItem* nItem = GetSceneManager().GetFade()->GetLoadScene()->GetEquipItemData();
+	//↓これはレア度がNormalの武器からランダムで武器を取得するコード
+	int num = nItem->GetNormalEquipItemList(Random().GetRandSInt() % nItem->GetNormalEquipItemListSize()) ;
+	//武器のタイプの取得
+	weaponNumber = nItem->GetItemStatus_ItemId(num).WeaponType;
+	//武器の名前の取得
+	itemName = nItem->GetItemStatus_ItemId(num).ItemName;
+	//武器のUI情報の取得(文字列)
+	textureFileName = nItem->GetItemStatus(num).ItemSprite;
+	weaponAttack = nItem->GetItemStatus_ItemId(num).ItemEffect;
+	*/
 	
 	if (weaponNumber == EnPlayerWeapon::enWeaponSword)
 	{
