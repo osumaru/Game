93f478336ce14840dc82209a68442c791c192a43
konishi <a>
2019-02-08 12:27:46 +0900

    エネミーがスタン状態から死亡状態に遷移したとき座標がずれていたので修正

--- Game/Game/Enemy/EnemyState/EnemyStan.cpp
+++ Game/Game/Enemy/EnemyState/EnemyStan.cpp
@@ -23,6 +23,14 @@ bool CEnemyStan::Start()
 
 void CEnemyStan::Update()
 {
+	//剛体の座標を更新する
+	const CMatrix* spineMatrix = m_enemy->GetWorldMatrixSpine();
+	CVector3 position;
+	position.x = spineMatrix->m[3][0];
+	position.y = spineMatrix->m[3][1];
+	position.z = spineMatrix->m[3][2];
+	m_enemy->GetCharacterController().SetPosition(position);
+
 	//ダメージを受けたか判定する
 	if (m_enemy->GetIsDamage())
 	{
@@ -40,26 +48,15 @@ void CEnemyStan::Update()
 		m_timer += GameTime().GetDeltaFrameTime();
 	}
 
-	if (m_timer > 3.0f)
+	const int DOWN_TIME = 3.0f;
+	if (m_timer > DOWN_TIME)
 	{
 		//移動しているので座標を更新
-		const CMatrix* spineMatrix = m_enemy->GetWorldMatrixSpine();
-		CVector3 position;
-		position.x = spineMatrix->m[3][0];
-		position.y = spineMatrix->m[3][1];
-		position.z = spineMatrix->m[3][2];
 		m_enemy->SetPosition(position);
 		//タイマーが一定値を超えたら立ち上がる
 		m_enemy->GetAnimation().Play(CEnemyState::enAnimation_StandUp);
 		m_timer = 0.0f;
 	}
-
-	const CMatrix* spineMatrix = m_enemy->GetWorldMatrixSpine();
-	CVector3 position;
-	position.x = spineMatrix->m[3][0];
-	position.y = spineMatrix->m[3][1];
-	position.z = spineMatrix->m[3][2];
-	m_enemy->GetCharacterController().SetPosition(position);
 
 	if (!m_enemy->GetAnimation().IsPlay()
 		&& m_enemy->GetAnimation().GetCurrentAnimationNum() == CEnemyState::enAnimation_StandUp) 
